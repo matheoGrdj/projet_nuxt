332e769c0c2e21e548a40165c59137c17450f747
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-05-04 17:42:25 +0200

    automatisation avec docker

--- application_projet/server/api/db.ts
+++ application_projet/server/api/db.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pgp = pgPromise({});
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const db = pgp(connectionString);
 
